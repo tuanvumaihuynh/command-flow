86d11247ac0125497f662d93a4324fc9406c83f6
nguyen083 <billbatri085@gmail.com>
2025-05-14 15:03:04 +0700

    feat: add emergemcy

---
+++ src/api/emergency.ts
@@ -0,0 +1,24 @@
+import type { RaybotHTTPClient } from '@/lib/http'
+import type { EmergencyState } from '@/types/emergency'
+
+export class EmergencyAPI {
+  client: RaybotHTTPClient
+
+  constructor(client: RaybotHTTPClient) {
+    this.client = client
+  }
+
+  getEmergencyState(): Promise<EmergencyState> {
+    return this.client.get('/emergency/state', {
+      doNotShowLoading: true,
+    })
+  }
+
+  stopEmergency(): Promise<void> {
+    return this.client.post('/emergency/stop')
+  }
+
+  resumeEmergency(): Promise<void> {
+    return this.client.post('/emergency/resume')
+  }
+}
